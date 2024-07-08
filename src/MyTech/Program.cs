3ab7425b1a875ed13761f3672d03e1079e08d01b
Justin Solo <justinpaulosolo@outlook.com>
2024-07-07 22:19:28 -0700

    feat: Add identity endpoints

--- src/MyTech/Program.cs
+++ src/MyTech/Program.cs
@@ -1,9 +1,18 @@
 using MyTech;
+using MyTech.Domain;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSqlite<ApplicationDbContext>(
     builder.Configuration.GetConnectionString("Sqlite"));
 
+builder.Services.AddAuthorization();
+
+builder.Services.AddIdentityApiEndpoints<User>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
 var app = builder.Build();
+
+app.MapIdentityApi<User>();
+
 app.Run();
