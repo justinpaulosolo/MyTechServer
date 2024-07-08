7fdd6474e9043c35b5980332ac5aaf182ea9aad2
Justin Solo <justinpaulosolo@outlook.com>
2024-07-07 22:16:20 -0700

    feat: Add Sqlite connection

--- src/MyTech/Program.cs
+++ src/MyTech/Program.cs
@@ -1,3 +1,9 @@
+using MyTech;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSqlite<ApplicationDbContext>(
+    builder.Configuration.GetConnectionString("Sqlite"));
+
 var app = builder.Build();
 app.Run();
